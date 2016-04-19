93b8ddce1b026166d7f4eb24bab83ddfa33b8dfa
Dominic Ferrantelli <dominic@ferrantel.li>
2016-04-19 16:48:39 -0700

    use Facter::Util instead of Facter::Core for our old facter

--- lib/facter/is_master.rb
+++ lib/facter/is_master.rb
@@ -2,8 +2,8 @@ require 'json';
 
 Facter.add('mongodb_is_master') do
   setcode do
-    if Facter::Core::Execution.which('mongo') 
-      mongo_output = Facter::Core::Execution.exec('mongo --quiet --eval "load(\'/root/.mongorc.js\'); printjson(db.isMaster())"')
+    if Facter::Util::Resolution.which('mongo') 
+      mongo_output = Facter::Util::Resolution.exec('mongo --quiet --eval "load(\'/root/.mongorc.js\'); printjson(db.isMaster())"')
       JSON.parse(mongo_output.gsub(/ISODate\((.+?)\)/, '\1 '))['ismaster'] ||= false
     else 
       'not_installed'
