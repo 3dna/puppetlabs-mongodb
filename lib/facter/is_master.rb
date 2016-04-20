b2c7d5668ca3c0981ed23ae51616427e8318951a
Dominic Ferrantelli <dominic@ferrantel.li>
2016-04-19 17:02:48 -0700

    remove ref to nonexistent file

--- lib/facter/is_master.rb
+++ lib/facter/is_master.rb
@@ -3,7 +3,7 @@ require 'json';
 Facter.add('mongodb_is_master') do
   setcode do
     if Facter::Util::Resolution.which('mongo') 
-      mongo_output = Facter::Util::Resolution.exec('mongo --quiet --eval "load(\'/root/.mongorc.js\'); printjson(db.isMaster())"')
+      mongo_output = Facter::Util::Resolution.exec('mongo --quiet --eval "printjson(db.isMaster())"')
       JSON.parse(mongo_output.gsub(/ISODate\((.+?)\)/, '\1 '))['ismaster'] ||= false
     else 
       'not_installed'
